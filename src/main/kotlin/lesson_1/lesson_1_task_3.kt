13f232120dad3b8714fa18b1de3adc4ffa194278
svetlanaza <svvetlanaz.dev@gmail.com>
2023-01-24 14:11:15 +0300

    KS-1-3 decision third task

---
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -0,0 +1,18 @@
+package lesson_1
+
+fun main() {
+    val yearFlight = 1961
+    var hour = 9
+    var minute = 7
+
+    println(yearFlight)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    println(yearFlight)
+    print(hour)
+    print(":")
+    println(minute)
+}
