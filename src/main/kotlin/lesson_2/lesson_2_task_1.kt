de53e761207b0aa7059bca89fa18a111775631e1
svetlanaza <svvetlanaz.dev@gmail.com>
2023-01-24 19:31:18 +0300

    KS-2-1 decision first task

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,16 @@
+package lesson_2
+
+fun main() {
+    val a = 3
+    val b = 4
+    val c = 3
+    val d = 5
+
+    println((a + b + c + d) /4.0)
+
+//    val gradePerson1 = 3f
+//    val gradePerson2 = 4f
+//    val gradePerson3: Float = gradePerson1
+//    val gradePerson4 = 5f
+//    println((gradePerson1 + gradePerson2 + gradePerson3 + gradePerson4) / 4)
+}
