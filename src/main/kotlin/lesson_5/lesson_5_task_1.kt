e7eb765f93a2b5fe9e6deca7e4da7324db996e72
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-01 17:46:22 +0300

    KS-5-1 decision first task

---
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -0,0 +1,17 @@
+package lesson_5
+
+fun main() {
+
+    println(NUMBER_1)
+    println(NUMbER_2)
+
+    val numberResult = readLine()!!.toInt()
+
+    if (numberResult == NUMBER_1 + NUMbER_2) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ закрыт!")
+    }
+}
+const val NUMBER_1 = 7
+const val NUMbER_2 = 4
