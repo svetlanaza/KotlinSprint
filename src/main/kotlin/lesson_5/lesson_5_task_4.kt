230a0525060330a3e48aa05691fd05437f2e5989
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-02 11:22:02 +0300

    KS-5-4 decision fourth task

---
+++ src/main/kotlin/lesson_5/lesson_5_task_4.kt
@@ -0,0 +1,22 @@
+package lesson_5
+
+fun main() {
+    println("Введите логин")
+
+    val login = readLine()
+
+    val resultText = if (login != LOGIN) "Пожалуйств, зарегистрируйтесь!"
+    else "Введите пороль"
+
+    println(resultText)
+
+    val password = readLine()
+
+    val resultMessage = if (password == PASSWORD) "Приветствуем вас, Batman!"
+    else "Ошибка авторизации"
+
+    println(resultMessage)
+}
+
+const val LOGIN = "Batman"
+const val PASSWORD = "password"
