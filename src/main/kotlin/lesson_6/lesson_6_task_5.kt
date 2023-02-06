97e39d9f3bd11d82376e905648c7ef582d8322c7
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-06 10:53:25 +0300

    KS-6-5 задача на авторизацию, испавление ошибок

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -1,26 +1,26 @@
 package lesson_6
 
 fun main() {
-    println("Здравствуйте! Сложите два числа")
+    println("Здравствуйте!")
+
     var counter = 3
     do {
 
         var number1 = (1..9).random()
-        println(number1)
         var number2 = (1..9).random()
-        println(number2)
+
+        println("Сложите $number1 и $number2")
 
         val result = readLine()!!.toInt()
 
         if (result == number1 + number2) {
             println("Добро пожаловать!")
         } else {
-            println("Неправильно, осталось ${--counter} попытки/попыток!\nСложите два числа")
+            println("Неправильно, осталось ${--counter} попытки/попыток!")
         }
 
         if (result == number1 + number2) {
             return
-            println("Добро пожаловать!")
         }
     } while (counter > 0)
 
