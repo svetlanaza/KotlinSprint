b0d022db895b90c3b3e645198e241fcfdc203a64
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-15 13:02:29 +0300

    KS-8-1 создание массива с данными просмотров рекламы

---
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -0,0 +1,7 @@
+package lesson_8
+
+fun main() {
+    val arrayOfView = arrayOf(4, 6, 9, 12, 5, 11, 20)
+    val result = arrayOfView[0] + arrayOfView[1] + arrayOfView[2] + arrayOfView[3] + arrayOfView[4] +arrayOfView[5] + arrayOfView[6]
+    println(result)
+}
