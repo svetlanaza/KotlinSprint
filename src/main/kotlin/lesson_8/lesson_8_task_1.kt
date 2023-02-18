83c908968f5091d024fcab65ab8d6f0682b37da6
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-18 09:57:54 +0300

    KS-8-1 создание массива для подсчета рекламы

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -2,6 +2,6 @@ package lesson_8
 
 fun main() {
     val arrayOfView = arrayOf(4, 6, 9, 12, 5, 11, 20)
-    val result = arrayOfView[0] + arrayOfView[1] + arrayOfView[2] + arrayOfView[3] + arrayOfView[4] +arrayOfView[5] + arrayOfView[6]
-    println(result)
+    val sum  = arrayOfView.sum()
+    println("Просмотры за неделю $sum")
 }
