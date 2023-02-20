99ebcc3ba4ee98ddd823eac863d249702a55bf15
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-20 10:05:41 +0300

    KS-8-5 создание массива для рецепта пользователя version 2

--- src/main/kotlin/lesson_8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -4,19 +4,30 @@ fun main() {
     println("Сколько ингредиентов будет в вашем блюде?")
     val ingredientsNumber = readLine()!!.toInt()
     var counter = ingredientsNumber
-    val dish = Array<String>(ingredientsNumber) { "" }
+
 
     while (counter > 0) {
         println("Введите ингредиент")
-        var ingredient = readLine()!!.toString()
+        val ingredient = readLine()!!.toString()
         counter--
+
+
     }
+    var dish = Array<String>(ingredientsNumber) { String() }
     println("В вашем блюде такие ингредиенты:")
-    while (counter > 0) {
-        println(dish)
 
-        counter--
+    for (i in 0 until dish.size) {
+
+        println(dish[i])
 
     }
+
 }
+
+
+
+
+
+
+
 
