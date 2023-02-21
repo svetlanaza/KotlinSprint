7fc37f06b60e9d1aa1a7ac4ff2ef39821a722fd1
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-21 16:53:13 +0300

    KS-9-5 создание cписка  рецепта из 5 элементов исправление

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -2,13 +2,13 @@ package lesson_9
 
 fun main() {
     val dish = mutableListOf<String>()
+
     for (i in 1..5) {
         println("Введите ингредиент")
-
         dish.add(readln().toString())
+    }
         dish.sort()
 
         print(dish[0].capitalize() + ", " + dish[1] + ", " + dish[2] + ", " + dish[3] + ", " + dish[4] + ".")
 
     }
-}
