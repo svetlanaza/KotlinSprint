609ecf3f4e269a0acad5e182cc62fc5dbafb80df
svetlanaza <svvetlanaz.dev@gmail.com>
2023-02-21 14:47:47 +0300

    KS-9-4 создание cписка  рецепта пользователя

--- src/main/kotlin/lesson_9/lesson_9_task_4.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_4.kt
@@ -4,18 +4,33 @@ fun main() {
     val dish = mutableListOf<String>()
     println("Введите 5 названий ингредиентов через запятую")
 
+    val list = readLine()!!.toString()
+    val delim = "\n"
+    list.split(delim)
 
     for (i in 1..5) {
         dish.add(readLine().toString())
-
     }
-
-
     dish.sort()
+
     dish.forEach {
         println(it)
     }
 
 
 
-}
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
