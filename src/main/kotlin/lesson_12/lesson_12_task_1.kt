94b78baeba57f47f01b0887770fd47c9064326e6
svetlanaza <svvetlanaz.dev@gmail.com>
2023-03-29 11:36:07 +0300

    KS-12-1 создание класса погоды, с конструктором, версия 2

--- src/main/kotlin/lesson_12/lesson_12_task_1.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -20,5 +20,18 @@ fun main() {
     println("Погода на 19 мая:")
     may19.printWeather()
 
+}
+class Weather() {
+    var temperatureDay = 20
+    var temperatureNight = 12
+    var precipitation = "Дождь"
+    var atmospherePressure = 750
+
+    fun printWeather() {
+        println("Дневная температура: $temperatureDay градусов")
+        println("Ночная температура: $temperatureNight градусов")
+        println("Осадки: $precipitation")
+        println("Атмосферное давление: $atmospherePressure мм")
+    }
 
 }
