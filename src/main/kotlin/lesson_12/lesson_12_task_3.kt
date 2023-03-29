ca7ef3f95a8cb20829ed5400950331e5940898f4
svetlanaza <svvetlanaz.dev@gmail.com>
2023-03-29 11:56:20 +0300

    KS-12-3 создание класса погоды, упрощенная запись. Исправлено форматирование

--- src/main/kotlin/lesson_12/lesson_12_task_3.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_3.kt
@@ -1,12 +1,28 @@
 package lesson_12
 
 fun main() {
-    val date1 = Day("21 апреля",26,20," Ясно", 750)
+    val date1 = Day("21 апреля", 26, 20, " Ясно", 750)
 
     date1.printWeather()
 
-    val date2 = Day("22 апреля", 22,18,"Дождь",730)
+    val date2 = Day("22 апреля", 22, 18, "Дождь", 730)
 
     println()
     date2.printWeather()
+}
+
+class Day(
+    var date: String,
+    var temperDay: Int,
+    var temperNight: Int,
+    var precipitation: String = "Дождь",
+    var pressure: Int
+) {
+    fun printWeather() {
+        println("Прогноз погоды на $date: ")
+        println("Температура днем $temperDay градусов")
+        println("Температура ночью $temperNight градусов")
+        println("Осадки - $precipitation")
+        println("Атмосферное давление - $pressure мм")
+    }
 }
