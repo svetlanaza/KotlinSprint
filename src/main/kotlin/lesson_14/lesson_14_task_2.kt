29fd60854c30bc5ae3fc783c197a2983f7df6f49
svetlanaza <svvetlanaz.dev@gmail.com>
2023-04-11 12:51:52 +0300

    KS-14-2 создание классов кораблей. изменен нейминг функций

--- src/main/kotlin/lesson_14/lesson_14_task_2.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_2.kt
@@ -4,41 +4,45 @@ open class Ship(
     val name: String,
     val speed: Int,
 
-) {
-    fun movement() {
+    ) {
+    fun printSpeedMovement() {
         println("\"$name\" движется со скоростью $speed узлов.")
     }
 }
+
 class Cargo(
     name: String,
     speed: Int,
-): Ship(name,speed) {
+) : Ship(name, speed) {
     val capacity = 200
     val spaciousness = 400
-    fun infoCargo() {
+    fun printInfoCargo() {
         println("Может перевозить груз массой до $capacity тонн, объемом $spaciousness литров.\n")
     }
 }
-class Icebreaker (
+
+class Icebreaker(
     name: String,
     speed: Int,
-): Ship(name,speed) {
+) : Ship(name, speed) {
     val weight = 21000
     val length = 150
-    fun specialFunctional() {
+    fun breaksIce() {
         println("Может колоть лед.")
     }
+
     fun infoIcebreaker() {
         println("Имеет водоизмещение $weight тонн и длину $length метров.")
-        specialFunctional()
+        breaksIce()
     }
 }
+
 fun main() {
     val cargo1 = Ship("Адмирал", 20)
-    cargo1.movement()
-    Cargo("Адмирал", 20).infoCargo()
+    cargo1.printSpeedMovement()
+    Cargo("Адмирал", 20).printInfoCargo()
     val icebreaker1 = Ship("Профессор Шварценгольд", 10)
-    icebreaker1.movement()
+    icebreaker1.printSpeedMovement()
     Icebreaker("Профессор Шварценгольд", 10).infoIcebreaker()
 
 }
