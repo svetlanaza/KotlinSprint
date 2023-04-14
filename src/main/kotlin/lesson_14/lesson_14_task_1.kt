a4e8933a508ee5a3a90f84bf7f71e690818376d3
svetlanaza <svvetlanaz.dev@gmail.com>
2023-04-14 17:55:41 +0300

    KS-14-1 создание классов кораблей с полями в конструкторе и скобочкой

--- src/main/kotlin/lesson_14/lesson_14_task_1.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_1.kt
@@ -1,9 +1,10 @@
 package lesson_14
 
-class StandardShip
-    (val name: String,
+class StandardShip(
+    val name: String,
     val speed: Int = 20,
-    val capacity: Int = 200,) {
+    val capacity: Int = 200,
+) {
     fun movement() {
         println("$name движется со скоростью $speed узлов.")
     }
