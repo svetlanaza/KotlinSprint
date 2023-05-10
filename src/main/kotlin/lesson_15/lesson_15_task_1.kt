687cebc04ff7fce4cb4c5c9f4492e09a30fab934
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-10 12:20:05 +0300

    KS-15-1 создание интерфейсов для передвижения существ

---
+++ src/main/kotlin/lesson_15/lesson_15_task_1.kt
@@ -0,0 +1,46 @@
+package lesson_15
+
+interface Flying {
+    fun fly() {
+    }
+}
+
+interface Swimming {
+    fun swim() {
+    }
+}
+
+class Seagull : Flying {
+    override fun fly() {
+        println("Чайка умеет летать.")
+    }
+}
+
+class Crucian : Swimming {
+    override fun swim() {
+        println("Карась уммеет плавать.")
+    }
+}
+
+class Duck : Flying, Swimming {
+    override fun fly() {
+        println("Утка умеет летать.")
+    }
+
+    override fun swim() {
+        println("Утка уммеет плавть.")
+    }
+
+}
+
+fun main() {
+    val seagull = Seagull()
+    seagull.fly()
+
+    val crucian = Crucian()
+    crucian.swim()
+
+    val duck = Duck()
+    duck.fly()
+    duck.swim()
+}
