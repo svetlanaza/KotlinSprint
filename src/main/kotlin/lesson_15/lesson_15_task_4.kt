bb0006142f9fb4ad5a2b07514210677701663f90
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-11 21:50:18 +0300

    KS-15-4 создание классов и интерфейса для поиска товара в магазине. Исправление форматирования

--- src/main/kotlin/lesson_15/lesson_15_task_4.kt
+++ src/main/kotlin/lesson_15/lesson_15_task_4.kt
@@ -5,12 +5,15 @@ interface Search {
         println("Выполняется поиск товара.")
     }
 }
+
 abstract class Product(name: String, number: Int) {
     val name = name
     val number = number
 }
+
 class Instrument(name: String, number: Int) : Product(name, number), Search
 class Accessories(name: String, number: Int) : Product(name, number), Search
+
 fun main() {
     val guitar = Instrument("Гитара", 14)
     val item = Accessories("Струны", 5)
@@ -19,6 +22,6 @@ fun main() {
     println("${guitar.name} - ${guitar.number} шт")
 
     guitar.searchProduct()
-    println("${item.name } - ${item.number} шт")
+    println("${item.name} - ${item.number} шт")
 
 }
