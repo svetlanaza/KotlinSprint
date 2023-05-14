669e4656f3d734cea571415f7029fe29b95148a7
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-14 18:15:18 +0300

    KS-5-5 Создание алгоритма лотереи

--- src/main/kotlin/lesson_5/lesson_5_task_5.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -2,16 +2,9 @@ package lesson_5
 
 import kotlin.random.Random
 
-
 fun main() {
-
     val numberOne = (0..100).random()
-    //println(numberOne)
-    // для проверки работы программы
-
     val numberEnd = (0..100).random()
-    //println(numberEnd)
-    // для проверки работы программы
 
     println("Введите два числа от 1 до 100")
 
@@ -21,8 +14,8 @@ fun main() {
     if ((number1 == numberOne && number2 == numberEnd) || (number1 == numberEnd && number2 == numberOne)) {
         println("Ура! Вы выиграли!")
     } else if ((number1 == numberOne || number1 == numberEnd) || (number2 == numberEnd || number2 == numberOne)) {
-        println("Вы угадали 1 число")
+        println("Вы угадали 1 число и получаете утешительный приз!")
     } else {
-        println("Вы проиграли Выигрышные числа: $numberOne $numberEnd")
+        println("Вы проиграли. Выигрышные числа: $numberOne $numberEnd")
     }
 }
