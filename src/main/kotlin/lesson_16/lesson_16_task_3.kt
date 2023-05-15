d966c44f8397c2e387dcb7f852fdaca7fcbcb772
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-15 22:41:44 +0300

    KS-16-3 проверка правильности пароля

--- src/main/kotlin/lesson_16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -1,11 +1,15 @@
 package lesson_16
 
 class UserHaman(name: String, login: String) {
-    private val password = "Barmaley13"
 
-    fun checkPassword() {
-        if (password == "Barmaley13") {
-            println("Добро пожаловать")
+    private val password = "Barmaley13"
+    val name = name
+
+    fun greetingsEndCheckPassword() {
+        println("Введите пороль!")
+        val passwordEnter = readln()
+        if (passwordEnter == password) {
+            println("Добро пожаловать, $name!")
         } else {
             println("Неверный пароль")
         }
@@ -14,5 +18,5 @@ class UserHaman(name: String, login: String) {
 
 fun main() {
     val user = UserHaman("Alex", "bom@bom")
-    user.checkPassword()
+    user.greetingsEndCheckPassword()
 }
