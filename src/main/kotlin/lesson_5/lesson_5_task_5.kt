41553fd56df9e9e58f1dd92b82f8eee56f583ac5
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-15 21:41:45 +0300

    KS-5-5 Создание алгоритма лотереи. разные комбинации

--- src/main/kotlin/lesson_5/lesson_5_task_5.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -4,8 +4,9 @@ import kotlin.random.Random
 
 fun main() {
     val numberOne = (0..100).random()
+    println(numberOne)
     val numberEnd = (0..100).random()
-
+    println(numberEnd)
     println("Введите два числа от 1 до 100")
 
     val number1 = readLine()!!.toInt()
@@ -13,9 +14,17 @@ fun main() {
 
     if ((number1 == numberOne && number2 == numberEnd) || (number1 == numberEnd && number2 == numberOne)) {
         println("Ура! Вы выиграли!")
-    } else if ((number1 == numberOne || number1 == numberEnd) || (number2 == numberEnd || number2 == numberOne)) {
-        println("Вы угадали 1 число и получаете утешительный приз!")
-    } else {
-        println("Вы проиграли. Выигрышные числа: $numberOne $numberEnd")
+    } else if ((number1 == numberOne) || (number2 != numberEnd)) {
+        println("Вы угадали 1 число и получаете утешительный приз! Выигрышные числа: $numberOne $numberEnd")
+    } else if ((number1 != numberOne) || (number2 == numberOne)) {
+        println("Вы угадали 1 число и получаете утешительный приз! Выигрышные числа: $numberOne $numberEnd")
+    } else if ((number1 == numberEnd) || (number2 != numberEnd)) {
+        println("Вы угадали 1 число и получаете утешительный приз! Выигрышные числа: $numberOne $numberEnd")
+    } else if (numberOne == number1 || numberOne == number2) {
+        if (numberEnd == number1 || numberEnd == number2) {
+
+        } else {
+            println("Вы проиграли. Выигрышные числа: $numberOne $numberEnd")
+        }
     }
 }
