b7825ca84595b4416d59d3c5c96e4b44a970fe1b
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-16 17:31:03 +0300

    KS-5-5 создание игры с угадыванием чисел

--- src/main/kotlin/lesson_5/lesson_5_task_5.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_5.kt
@@ -1,30 +1,21 @@
 package lesson_5
 
-import kotlin.random.Random
-
 fun main() {
-    val numberOne = (0..100).random()
-    println(numberOne)
-    val numberEnd = (0..100).random()
-    println(numberEnd)
+    val numberFirst = (0..100).random()
+    val numberSecond = (0..100).random()
+
     println("Введите два числа от 1 до 100")
 
-    val number1 = readLine()!!.toInt()
-    val number2 = readLine()!!.toInt()
+    val number1 = readln().toInt()
+    val number2 = readln().toInt()
 
-    if ((number1 == numberOne && number2 == numberEnd) || (number1 == numberEnd && number2 == numberOne)) {
+    if ((number1 == numberFirst && number2 == numberSecond) || (number1 == numberSecond && number2 == numberFirst)) {
         println("Ура! Вы выиграли!")
-    } else if ((number1 == numberOne) || (number2 != numberEnd)) {
-        println("Вы угадали 1 число и получаете утешительный приз! Выигрышные числа: $numberOne $numberEnd")
-    } else if ((number1 != numberOne) || (number2 == numberOne)) {
-        println("Вы угадали 1 число и получаете утешительный приз! Выигрышные числа: $numberOne $numberEnd")
-    } else if ((number1 == numberEnd) || (number2 != numberEnd)) {
-        println("Вы угадали 1 число и получаете утешительный приз! Выигрышные числа: $numberOne $numberEnd")
-    } else if (numberOne == number1 || numberOne == number2) {
-        if (numberEnd == number1 || numberEnd == number2) {
-
-        } else {
-            println("Вы проиграли. Выигрышные числа: $numberOne $numberEnd")
+    } else if ((number1 == numberFirst) && (number2 != numberSecond)) {
+        println("Вы угадали 1 число и получаете утешительный приз! Второе число: $numberSecond")
+    } else if ((number2 == numberSecond) && (number1 != numberFirst)) {
+        println("Вы угадали 1 число и получаете утешительный приз! Второе число: $numberFirst")
+    } else {
+            println("Вы проиграли. Выигрышные числа: $numberFirst $numberSecond")
         }
     }
-}
