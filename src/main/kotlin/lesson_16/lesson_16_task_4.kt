5ff331cd306c7b6ef1edd03eac62d7217a8ff9b7
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-16 11:14:19 +0300

    KS-16-4 создание класса заказа для интернет магазина

--- src/main/kotlin/lesson_16/lesson_16_task_4.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_4.kt
@@ -1,22 +1,29 @@
 package lesson_16
 
-class Order {
-    var statusReadiness = false
+class Order(var statusReadiness: Boolean) {
+
     private val numberOrder = 1
 
     fun orderInfo() {
         println("Номер закза $numberOrder")
+        if (statusReadiness == false) {
+            println("Заказ обрабатывается")
+        } else {
+            println("Заказ готов")
+        }
     }
 
-    //fun  changeStatus()
-    fun requestOnChange(newStatus: Boolean) {
-        println("Заказ готов!")
-
+    fun orderChange() {
+        println("Менеджер оставил заявку об изменении статуса заказа")
     }
+
 }
 
 fun main() {
-    val order = Order()
+    var order = Order(false)
     order.orderInfo()
-    order.requestOnChange(true)
+    order.orderChange()
+    order = Order(true)
+    order.orderInfo()
+
 }
