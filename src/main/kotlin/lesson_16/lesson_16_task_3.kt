70c863f4e7ee7671cecd11d79b4288db3bf049ee
svetlanaza <svvetlanaz.dev@gmail.com>
2023-05-17 18:24:13 +0300

    KS-16-3 создание класса пользователя. изменила функцию

--- src/main/kotlin/lesson_16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -1,19 +1,20 @@
 package lesson_16
 
-class UserHaman(name: String, login: String, private val password: String) {
-
-    fun checkPassword() {
-        println("Введите пороль!")
-        val passwordEnter = readln()
-        if (passwordEnter == password) {
-            println("Верный пароль")
-        } else {
-            println("Неверный пароль")
-        }
+class UserHaman(name: String, login: String, pass: String) {
+    val name = name
+    val login = login
+    private val pass = pass
+    fun checkPassword(password: String): Boolean {
+        return pass == password
     }
 }
 
 fun main() {
     val user = UserHaman("Alex", "bom@bom", "qwerty48")
-    user.checkPassword()
+
+    println(user.name)
+    println(user.login)
+    println(user.checkPassword("qwerty48"))
+    println(user.checkPassword("qwerty"))
+
 }
